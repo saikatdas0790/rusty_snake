58a3969ee73b0374a67b1151fd44f445594903a1
Saikat Das <saikatdas0790@gmail.com>
2022-10-06 22:01:04 +0530

    Multithreading

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::{thread, time::Duration};
+
 use sdl2::{
     event::Event, keyboard::Keycode, pixels::Color, render::Canvas, video::Window, EventPump,
 };
@@ -24,6 +26,8 @@ fn init(width: u32, height: u32) -> (Canvas<Window>, EventPump) {
 fn main() {
     let (canvas, mut events) = init(720, 720);
 
+    thread::spawn(move || {});
+
     'game: loop {
         for event in events.poll_iter() {
             match event {
@@ -32,8 +36,10 @@ fn main() {
                     keycode: Some(Keycode::Escape),
                     ..
                 } => break 'game,
-                _ => {}
+                _ => continue 'game,
             }
         }
+
+        thread::sleep(Duration::from_millis(800));
     }
 }
