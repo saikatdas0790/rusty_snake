728bebfdbd03657f0a2facbf0ca4e439521c244d
Saikat Das <saikatdas0790@gmail.com>
2022-10-06 12:04:54 +0530

    Cargo and Dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use sdl2::{pixels::Color, render::Canvas, video::Window, EventPump};
+
+fn init(width: u32, height: u32) -> (Canvas<Window>, EventPump) {
+    let sdl_context = sdl2::init().unwrap();
+    let video_subsystem = sdl_context.video().unwrap();
+
+    let window = video_subsystem
+        .window("Game", width + 1, height + 1)
+        .position_centered()
+        .build()
+        .unwrap();
+    let mut canvas = window.into_canvas().present_vsync().build().unwrap();
+
+    canvas.set_draw_color(Color::RGB(0, 0, 0));
+    canvas.clear();
+    canvas.present();
+
+    let event_pump = sdl_context.event_pump().unwrap();
+    (canvas, event_pump)
+}
+
 fn main() {
     println!("Hello, world!");
 }
